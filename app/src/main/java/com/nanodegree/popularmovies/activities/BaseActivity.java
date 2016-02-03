74ad2e40d7ffb1c1ad8bef5ecd253939a0e2f19a
Yogesh <yogeshmadaan100@gmail.com>
2016-02-03 15:29:35 +0530

    Inital commit, Base activity and gitignore added

---
+++ app/src/main/java/com/nanodegree/popularmovies/activities/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.nanodegree.popularmovies.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
